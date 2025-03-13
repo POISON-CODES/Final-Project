58046f96b66e37478c154d6b1ce9ffe364a4ad01
Aman Keswani <amanskes@gmail.com>
2025-03-13 18:18:36 +0530

    transfer testing

--- docker_backup.sh
+++ docker_backup.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+# Backup Appwrite Data
 docker exec appwrite tar -czvf /storage/appwrite_backup.tar.gz /storage
 docker cp appwrite:/storage/appwrite_backup.tar.gz .
+
+echo "Backup completed: appwrite_backup.tar.gz saved."
--- docker_restore.sh
+++ docker_restore.sh
@@ -1,2 +1,10 @@
+
+
+
+#!/bin/bash
+
+# Backup Appwrite Data
 docker cp appwrite_backup.tar.gz appwrite:/storage/
 docker exec appwrite tar -xzvf /storage/appwrite_backup.tar.gz -C /
+
+echo "Backup completed: appwrite_backup.tar.gz saved."
